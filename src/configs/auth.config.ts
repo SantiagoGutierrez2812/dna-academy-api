0a06d2bb4ba102e0b96b7e878ec8bb35bce26535
Santiago <santiago.gutierrezyg@gmail.com>
2025-12-15 16:37:01 -0500

    fixed: arreglo de cookies

--- src/configs/auth.config.ts
+++ src/configs/auth.config.ts
@@ -11,7 +11,7 @@ export const AUTH_CONFIG: AuthConfig = {
         cookieOptions: {
             httpOnly: true,
             secure: appConfig.NODE_ENV === "production",
-            sameSite: appConfig.NODE_ENV === "production" ? "none" : "lax",
+            sameSite: appConfig.NODE_ENV === "production" ? "none" : "none",
             maxAge: appConfig.JWT_ACCESS_EXP_MINUTES * 60 * 1000, // 15 minutos por defecto
             path: "/",
         }
@@ -24,7 +24,7 @@ export const AUTH_CONFIG: AuthConfig = {
         cookieOptions: {
             httpOnly: true,
             secure: appConfig.NODE_ENV === "production",
-            sameSite: appConfig.NODE_ENV === "production" ? "none" : "lax",
+            sameSite: appConfig.NODE_ENV === "production" ? "none" : "none",
             maxAge: appConfig.JWT_REFRESH_EXP_DAYS * 24 * 60 * 60 * 1000, // 7 días por defecto
             path: "/api/auth",
         }
